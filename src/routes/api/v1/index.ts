37e872e4a21a7eb6aa4b98faea08bccaea3eceb6
Renz <git@mihou.dev>
2025-05-29 14:24:08 +0800

    H-3: Add /auth/login route

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,8 +1,14 @@
 import {Hono} from "hono";
 import {cliRoutes} from "./cli";
 import {updatesRoutes} from "./updates";
+import {useBasicAuth} from "../../../middlewares/validate";
 
 export const apiV1 = new Hono().basePath("/api/v1");
 
+apiV1.get(
+    "/auth/login",
+    useBasicAuth,
+    (c) => c.text("OK")
+)
 apiV1.route("", cliRoutes);
 apiV1.route("", updatesRoutes);
